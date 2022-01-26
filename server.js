d32154b2a3194664c722d0dfb1c705de30161e83
Tristan Budiman <29161072+trisbman@users.noreply.github.com>
2022-01-26 15:00:19 +0700

    Create public API

--- server.js
+++ server.js
@@ -4,10 +4,34 @@ const port = process.env.PORT || 3000
 const fs = require("fs")
 let data = fs.readFileSync('asian-countries.json')
 let items = JSON.parse(data);
+const cors = require("cors")
 
 const app = express()
 app.use(express.static('public'))
+app.use(cors())
 
+// API
+app.get('/asiancountries', allItems)
+
+function allItems(request, response) {
+  response.send(items)
+}
+app.get('/asiancountries/:asiancountry/', search);
+
+function search(request, response) {
+  let term = request.params.asiancountry;
+  let reply = null;
+  term = term.charAt(0).toUpperCase() + term.slice(1).toLowerCase();
+
+  if (items[term])
+    reply = items[term]
+  else
+    reply = { status: "Not Found" }
+
+  response.send(reply);
+}
+
+// View
 app.get('/', function (req, res) {
   ejs.renderFile('views/home.ejs', { items }, {}, (err, template) => {
     if (err) throw err
