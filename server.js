4f2cff3eacb61fcb5870569fc74a6a9d93898e6c
Tristan Budiman <29161072+trisbman@users.noreply.github.com>
2022-01-26 14:23:20 +0700

    Add mock API

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const ejs = require('ejs')
 const port = process.env.PORT || 3000
-let data = ['data 1', 'data 2', 'data 3']
+const fs = require("fs")
+let data = fs.readFileSync('asian-countries.json')
+let items = JSON.parse(data);
 
 const app = express()
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  ejs.renderFile('views/home.ejs', {data}, {}, (err, template) => {
-      if (err) throw err
-      res.end(template)
+  ejs.renderFile('views/home.ejs', { items }, {}, (err, template) => {
+    if (err) throw err
+    res.end(template)
   })
 })
 
-app.listen(port, function(err) {
-	if(err) throw err
-	console.log('Server running on port '+port)
+app.listen(port, function (err) {
+  if (err) throw err
+  console.log('Server running on port ' + port)
 })
