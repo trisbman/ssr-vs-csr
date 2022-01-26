d17219e6eb982fce57fdc05384979d3d3d2512d9
Tristan Budiman <29161072+trisbman@users.noreply.github.com>
2022-01-26 15:39:24 +0700

    Create CSR version

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+const listTabDiv = $("#list-tab")
+const tabContentDiv = $('#nav-tabContent')
+$.getJSON("https://immense-plains-36763.herokuapp.com/asiancountries", data => {
+
+
+    $.each(data, (item) => {
+        // item index
+        const listTabItem = $(`<a class="list-group-item list-group-item-action" 
+        data-bs-toggle="list" role="tab"></a>`)
+        listTabItem.prop("href", "#list-" + item)
+        listTabItem.text(item)
+        listTabDiv.append(listTabItem)
+
+        // item content
+        const tabPane = $(`<div class="tab-pane fade show" role="tabpanel">`)
+        tabPane.prop("id", "list-" + item)
+        const listGroup = $(`<ul class="list-group"></ul>`)
+        const listGroupItem = [
+            $(`<li class="list-group-item">
+                Capital City: ${data[item].capital}
+            </li>`),
+            $(`<li class="list-group-item">
+                Currency: ${data[item].currency}
+            </li>`),
+            $(`<li class="list-group-item">
+                Language: ${data[item].lang}
+            </li>`)
+        ]
+        $.each(listGroupItem, (i, item) => listGroup.append(item))
+        tabPane.append(listGroup)
+        tabContentDiv.append(tabPane)
+    })
+})
